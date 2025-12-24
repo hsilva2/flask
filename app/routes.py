96361935c1b2aaad079057ebc483d76d4f5ffb43
hsilva2 <limateste19@gmail.com>
2025-12-24 13:26:09 -0400

    create the aplication

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template, request
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        return render_template('response.html', name=name, email=email, message=message)
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return render_template('contact.html', name=name, email=email)
+    return render_template('form.html')
